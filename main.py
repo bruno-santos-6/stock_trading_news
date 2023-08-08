8da3deea759b7193b90f8be43ef745d34da64e5f
brunodos-santos <bruno6.santos@usp.br>
2023-08-08 20:53:07 -0300

    I pulled the data from News API e formatted it into a message structure for later sending the SMS with the news

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import requests
 from dotenv import load_dotenv
 import os
 
-
 # -------------------- CONSTANTS --------------------
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
@@ -13,6 +12,7 @@ NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 # -------- LOADING THE ENVIRONMENT VARIABLES --------
 load_dotenv(".env")
 STOCK_API_KEY = os.getenv("ALPHA_VANTAGE_API_KEY")
+NEWS_API_KEY = os.getenv("NEWS_API")
 
 parameters = {
     "function": "TIME_SERIES_DAILY",
@@ -37,18 +37,19 @@ day_before_yesterday_closing_price = float(day_before_yesterday["4. close"])
 difference = abs(yesterday_closing_price - day_before_yesterday_closing_price)
 
 diff_percent = (difference / yesterday_closing_price) * 100
-print(diff_percent)
 
 if diff_percent > 0:
-    print("Get News")
 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
+    news_params = {
+        "apiKey": NEWS_API_KEY,
+        "qInTitle": COMPANY_NAME
+    }
 
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    articles = news_response.json()["articles"]
+
+    three_articles = articles[:3]
+
+    print(three_articles)
+
+    formatted_articles = [f"Headline: {article['title']}. \nBrief: {article['description']}" for article in three_articles]
